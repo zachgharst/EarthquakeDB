5cb5d3687f1af815df7e234b5107316dd67422a7
MattMiller1989 <57654274+MattMiller1989@users.noreply.github.com>
2020-11-15 16:41:06 -0600

    Add files via upload

--- ddl_rough_draft.sql
+++ ddl_rough_draft.sql
@@ -20,4 +20,32 @@ LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
 
-SELECT * FROM earthquake;
+CREATE TABLE city(
+id INT NOT NULL AUTO_INCREMENT,
+name varchar(50) NOT NULL,
+latitude FLOAT,
+longitude FLOAT,
+country varchar(50),
+population INT DEFAULT NULL,
+PRIMARY KEY(id)
+);
+
+DROP TABLE city;
+
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/worldcities2.csv'
+INTO TABLE city
+-- CHARACTER SET latin1
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+SELECT * FROM city
+WHERE population >1000000
+AND country='United States';
+
+SELECT id, name, SQRT((latitude - 40.69) + (longitude + 73.92)) as distance FROM city ORDER BY distance;
+
+SELECT id, name, SQRT((latitude - 40.69)*(latitude - 40.69) + (longitude + 73.92)*(longitude + 73.92)) as distance FROM city ORDER BY distance;
