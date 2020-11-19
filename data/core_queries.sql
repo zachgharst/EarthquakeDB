e71895d1eebbe74a45cca9dbeabfbdd654bd796f
MattMiller1989 <57654274+MattMiller1989@users.noreply.github.com>
2020-11-18 23:03:58 -0600

    Added another query

--- data/core_queries.sql
+++ data/core_queries.sql
@@ -1,5 +1,19 @@
+
+-- Returns a list of the closest earthquakes to a given city
 SELECT earthquake.id, mag, ST_Distance_Sphere(
     point(earthquake.longitude, earthquake.latitude),
     point(city.longitude, city.latitude )
 ) * .000621371192 AS distance  
 FROM earthquake INNER JOIN city ON name = 'Miami' ORDER BY distance;
+
+-- Returns a list of all the cities with population > X and distance <Y and magnitude >Z 
+SELECT city.name, earthquake.id, earthquake.mag, ST_Distance_Sphere(
+    point(earthquake.longitude, earthquake.latitude),
+    point(city.longitude, city.latitude )
+) * .000621371192 AS distance 
+FROM earthquake, city 
+WHERE mag > 5.0
+AND population >100000
+GROUP BY city.id
+HAVING distance <1000.0
+ORDER BY mag;
