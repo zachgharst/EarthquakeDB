d66fb9cea647818f70106cac5c64a30ad979334a
zach.gharst <zdgharst@gmail.com>
2020-11-25 11:44:47 -0600

    Add population aggregate for a circular area

--- data/core_queries.sql
+++ data/core_queries.sql
@@ -24,3 +24,10 @@ AND population >100000
 GROUP BY city.id
 HAVING distance <1000.0
 ORDER BY mag;
+
+-- Find the total population within x miles of a long/lat (example: 20 miles of New York).
+SELECT Sum(population) as Population
+FROM city
+WHERE St_distance_sphere(Point(-74.01, 40.71),
+	Point(longitude, latitude))
+	* .000621371192 < 20;
