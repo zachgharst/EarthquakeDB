abe73ebd7f013ce2cb2a01e97ca4dce8aab41b3f
zach.gharst <zdgharst@gmail.com>
2020-11-29 05:42:56 -0600

    Create trigger for when an earthquake is inserted

---
+++ data/triggers.sql
@@ -0,0 +1,13 @@
+DROP TRIGGER IF EXISTS ProduceDamage; 
+
+DELIMITER $$
+
+CREATE TRIGGER ProduceDamage
+AFTER INSERT
+ON `earthquake` FOR EACH ROW
+BEGIN
+	CALL GenerateRandomDamage(NEW.id, NEW.mag, NEW.latitude, NEW.longitude);
+END $$
+
+    
+DELIMITER ;
