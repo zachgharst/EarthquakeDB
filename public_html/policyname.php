66b568f6bdc885e8d245fa7da55bbc468980d005
AhmedBoukhousse <47191893+AhmedBoukhousse@users.noreply.github.com>
2020-11-29 21:35:47 -0600

    Update policyname.php

--- public_html/policyname.php
+++ public_html/policyname.php
@@ -3,14 +3,14 @@
     require_once('includes/db-config.php');
 
     $title = "Policy";
-    $query = "SELECT policy.name, policy.id, policy.cityId
+    $query = "SELECT policy_name, id, city_id
     FROM policy";
 
     $result = mysqli_query($connection, $query);
 
     while ($row = mysqli_fetch_array($result))
     {
-            $content .= "Policy Name: $row[name] | Policy ID: $row[id] | City: $row[cityId]";
+            $content .= "Policy Name: $row[policy_name] | Policy ID: $row[id] | City: $row[city_id]";
             $content .= "<br>";
    }
 
