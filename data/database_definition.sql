36d191d729dee06f183f40df5bcd51e83aff14b8
MattMiller1989 <57654274+MattMiller1989@users.noreply.github.com>
2020-12-02 01:22:36 -0600

    Update database_definition.sql

--- data/database_definition.sql
+++ data/database_definition.sql
@@ -64,10 +64,16 @@ PRIMARY KEY(cluster_id),
 FOREIGN KEY(earthquake_id) REFERENCES earthquake(id)
 );
 
+
 INSERT INTO `policy_type`
 VALUES (1,'Prime',0.5), (2, 'FullCoverage', 1.0), (3, 'Basic', 0.1), (4, 'Prime', 0.7), (5, 'Basic', 0.3), (6, 'Prime', 0.8), (7, 'FullCoverage', 1.5), 
 (8, 'Prime', 0.9), (9, 'Basic', 0.4), (10, 'Prime', 1.3);
 /* BEFORE ANY DATA CAN BE LOADED, THE STORED PROCEDURES AND TRIGGERS SHOULD BE CREATED. */
+
+INSERT INTO cluster
+SELECT DISTINCT clusterid, q1id FROM clusterview3;
+
+
 
 -- Matt
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/eqdata.csv'
