62fb697567b434b8882d358f2e616c9edfb92dc7
zach.gharst <zdgharst@gmail.com>
2020-12-02 16:54:02 -0600

    Create table of earthquakes for a city

--- public_html/city.php
+++ public_html/city.php
@@ -4,20 +4,41 @@
     $title = "Search by City";
     
     if(isset($_GET[id])) {
-        $id = $_GET[id];
+        $id = mysqli_real_escape_string($connection, $_GET[id]);
 
-        $getCityData = "SELECT name, latitude, longitude, country, population FROM city WHERE id = $id";
-        $getCityData = mysqli_query($connection, $getCityData);
-        $getCityData = mysqli_fetch_array($getCityData);
+        $cityData = "SELECT name, latitude, longitude, country, population FROM city WHERE id = $id";
+        $cityData = mysqli_query($connection, $cityData);
+        $cityData = mysqli_fetch_array($cityData);
 
 
-        if($getCityData) {
-            $content .= "<h2>$getCityData[name]</h2>
+        if($cityData) {
+            $EQData = "SELECT DATE_FORMAT(DATE(time), '%M %e, %Y') as time1, TIME(time) as time2, mag, earthquake.latitude, earthquake.longitude, ST_Distance_Sphere(
+                point(earthquake.longitude, earthquake.latitude),
+                point(city.longitude, city.latitude )
+            ) * .000621371192 AS distance  
+            FROM earthquake INNER JOIN city ON city.id = $id
+            HAVING distance < 50
+            ORDER BY distance;";
+            $EQData = mysqli_query($connection, $EQData);
+            $numEQ = mysqli_num_rows($EQData);
+
+
+            $content .= "<h2>$cityData[name]</h2>
             <ul>
-                <li>Country: $getCityData[country]</li>
-                <li>Population: $getCityData[population]</li>
-                <li>Location: ($getCityData[latitude], $getCityData[longitude])</li>
+                <li>Country: $cityData[country]</li>
+                <li>Population: $cityData[population]</li>
+                <li>Location: ($cityData[latitude], $cityData[longitude])</li>
+                <li>Number of Nearby Earthquakes This Year: $numEQ</li>
             </ul>";
+
+            if($numEQ > 0) {
+                $content .= "<table><th>Date</th><th>Time</th><th>Latitude</th><th>Longitude</th><th>Magnitude</th><th>Miles from City Center</th>";
+                while($row = mysqli_fetch_array($EQData)) {
+                    $row[distance] = round($row[distance], 1);
+                    $content .= "<tr class=\"right\"><td class=\"left\">$row[time1]</td><td>$row[time2]</td><td>$row[mag]</td><td>$row[latitude]</td><td>$row[longitude]</td><td>$row[distance]</td></tr>";
+                }
+                $content .= "</table>";
+            }
         }
 
         else {
