b751f4c85cd621484abca406864c49e7685ec815
zach.gharst <zdgharst@gmail.com>
2020-12-02 14:42:06 -0600

    Prevent SQL injection in city search box

--- public_html/city.php
+++ public_html/city.php
@@ -1,13 +1,17 @@
 <?php
 
     require_once('includes/db-config.php');
+    
+    if(isset($_GET[cityname])) {
+    
+    $cityname = mysqli_real_escape_string($connection, $_GET[cityname]);
 
     $title = "Cities";
     $query = "SELECT earthquake.id, mag, ST_Distance_Sphere(
         point(earthquake.longitude, earthquake.latitude),
         point(city.longitude, city.latitude )
     ) * .000621371192 AS distance  
-    FROM earthquake INNER JOIN city ON name = '$_GET[cityname]' 
+    FROM earthquake INNER JOIN city ON name = '$cityname' 
     WHERE mag > 5.0
     HAVING distance < 500
     ORDER BY distance;";
@@ -18,6 +22,10 @@
     {
             $content .= "EID: $row[id] | Mag: $row[mag] | Distance: $row[distance]";
             $content .= "<br>";
+   }
+   }
+   else {
+   $content = "Search please!";
    }
 
     include('includes/template.php');
