a83bd9012420466ff052c9ac8ababd699ebe1745
MattMiller1989 <57654274+MattMiller1989@users.noreply.github.com>
2020-12-02 01:15:27 -0600

    Update core_queries.sql

--- data/core_queries.sql
+++ data/core_queries.sql
@@ -54,3 +54,41 @@ WHERE ST_Distance_Sphere(
 ) * .000621371192 <50
 GROUP BY t1.id
 ORDER BY population DESC;
+	      
+
+	      
+	      
+	      
+--THIS NEXT SECTION SOLELY INVOLVES THE CREATION OF THE VIEWS NECESSARY TO CREATE THE CLUSTER TABLE:	      
+	      
+	      
+-- Creates a View that compares all of the Earthquakes above a 5.0 magnitude and looks for other quakes of 5.0 magnitude
+-- If within 5 miles, it stores both of the quakes into a "clusterview"
+-- We can rework the distance values if need be, but the magnitude should be rather high, as the time complexity for this can be pretty wonky	      
+CREATE VIEW clusterview AS
+SELECT t2.id AS q1id, t2.mag AS q1mag, t1.id AS q2id, t1.mag AS q2mag, ST_Distance_Sphere(
+    point(t1.longitude, t1.latitude),
+    point(t2.longitude, t2.latitude )
+) * .000621371192 AS distance 
+FROM (SELECT * FROM earthquake WHERE mag > 5.0) t1 CROSS JOIN (SELECT * FROM earthquake WHERE mag > 5.0) t2
+HAVING distance < 10
+AND t1.id !=t2.id;
+	      
+-- Next step removes duplicates, as a quake will appear in q1 as well as q2 later on in the list:
+CREATE  VIEW clusterview2 AS
+SELECT  least (q1id, q2id) q1id,
+        greatest(q1id, q2id) q2id
+FROM   clusterview7
+GROUP   BY         
+        least (q1id, q2id) ,
+        greatest(q1id, q2id); 
+	      
+-- Creates another view that uses 'dense_rank()' to properly assign a unique identifier to each new value of the 'q1' column from the previous view.
+-- This dense_rank value will later be used as our clusterid attribute
+-- We can use the data from this view for the insert statements that will go into our cluster table.
+	      
+CREATE VIEW clusterview3 AS
+SELECT dense_rank() over (order by q1id) AS clusterid ,q1id,q2id
+FROM clusterview2;
+	      
+	      
