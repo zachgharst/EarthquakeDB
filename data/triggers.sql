6a8b41a87d09efbf1513b474786bde1e75e1e583
MattMiller1989 <57654274+MattMiller1989@users.noreply.github.com>
2020-12-05 16:24:42 -0600

    Update triggers.sql

--- data/triggers.sql
+++ data/triggers.sql
@@ -10,3 +10,24 @@ BEGIN
 END $$
     
 DELIMITER ;
+
+
+CREATE TRIGGER CreateEarthquakeCity
+BEFORE INSERT
+ON `earthquake` FOR EACH ROW
+BEGIN
+	DECLARE radius FLOAT DEFAULT 0.0;
+   
+    SET radius= 2 * NEW.mag * NEW.mag;
+    
+    INSERT INTO earthquake_city (earthquake_id, city_id)
+    SELECT NEW.id, city.id
+        FROM   city INNER JOIN earthquake ON city.id=NEW.id
+        WHERE  St_distance_sphere(Point(NEW.longitude, NEW.latitude), Point(
+                    city.longitude, city.latitude)) * .000621371192 < radius;  
+   
+    
+    SET NEW.effected_population=populationInRadius;
+END $$
+    
+DELIMITER ;
