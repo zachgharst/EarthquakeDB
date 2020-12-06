2f037406285dadceabf6be70e2e08fc5da46464d
MattMiller1989 <57654274+MattMiller1989@users.noreply.github.com>
2020-12-05 22:51:20 -0600

    Update triggers.sql

--- data/triggers.sql
+++ data/triggers.sql
@@ -1,17 +1,3 @@
-DROP TRIGGER IF EXISTS ProduceDamage; 
-
-DELIMITER $$
-
-CREATE TRIGGER ProduceDamage
-AFTER INSERT
-ON `earthquake` FOR EACH ROW
-BEGIN
-	CALL GenerateRandomDamage(NEW.id);
-END $$
-    
-DELIMITER ;
-
-
 DROP TRIGGER IF EXISTS FindPopulation; 
 
 DELIMITER $$
@@ -21,7 +7,7 @@ BEFORE INSERT
 ON `earthquake` FOR EACH ROW
 BEGIN
 	DECLARE radius FLOAT DEFAULT 0.0;
-    DECLARE populationInRadius INT DEFAULT 0;
+    DECLARE populationInRadius FLOAT DEFAULT 0;
     
     SET radius= 2 * NEW.mag * NEW.mag;
     
@@ -30,11 +16,12 @@ BEGIN
         FROM   city
         WHERE  St_distance_sphere(Point(NEW.longitude, NEW.latitude), Point(
                     city.longitude, city.latitude)) * .000621371192 < radius;  
-   IF(populationInRadius IS NULL) THEN
+    IF(populationInRadius IS NULL) THEN
             SET populationInRadius = 0;
         END IF;
-    
     SET NEW.effected_population=populationInRadius;
+    
+    CALL GenerateRandomDamage(NEW.id);
 END $$
     
 DELIMITER ;
