7101c097bcfb9699ea295f27e86e9ce50caeb725
MattMiller1989 <57654274+MattMiller1989@users.noreply.github.com>
2020-12-05 23:45:06 -0600

    Create Trigger for junction table

--- data/triggers.sql
+++ data/triggers.sql
@@ -12,22 +12,27 @@ END $$
 DELIMITER ;
 
 
-CREATE TRIGGER CreateEarthquakeCity
-BEFORE INSERT
+DROP TRIGGER IF EXISTS CreateJunction; 
+
+DELIMITER $$
+
+CREATE TRIGGER CreateJunction
+AFTER INSERT
 ON `earthquake` FOR EACH ROW
 BEGIN
+
 	DECLARE radius FLOAT DEFAULT 0.0;
-   
+    DECLARE populationInRadius FLOAT DEFAULT 0;
+    
     SET radius= 2 * NEW.mag * NEW.mag;
-    
-    INSERT INTO earthquake_city (earthquake_id, city_id)
-    SELECT NEW.id, city.id
-        FROM   city INNER JOIN earthquake ON city.id=NEW.id
+
+	   INSERT INTO earthquake_city
+      SELECT NEW.id,city.id
+		FROM   city
         WHERE  St_distance_sphere(Point(NEW.longitude, NEW.latitude), Point(
-                    city.longitude, city.latitude)) * .000621371192 < radius;  
-   
+                    city.longitude, city.latitude)) * .000621371192 < radius;
     
-    SET NEW.effected_population=populationInRadius;
+    
 END $$
     
-DELIMITER ;
+    DELIMITER ;
