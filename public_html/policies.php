933f9dc9791340f4452259ea04e6b8c26c5b8ed4
AhmedBoukhousse <47191893+AhmedBoukhousse@users.noreply.github.com>
2020-12-06 23:22:51 -0600

    Update policies.php

--- public_html/policies.php
+++ public_html/policies.php
@@ -12,8 +12,8 @@
 $content .= "<table><tr><th class=\"left\">Policy Name</th><th class=\"left\">Policy Id</th><th class=\"right\">City</th></tr>";
     while ($row = mysqli_fetch_array($result))
     {
-            //$content .= "Policy Name: $row[policy_name] | Policy ID: $row[id] | City: $row[name]";
-         $content .= "<tr><td>$row[p_name]\$row[p_id]</td><td class=\"right\">$row[c_name]</td></tr>";
+           
+         $content .= "<tr><td class=\"left\">$row[p_name]</td><td>$row[p_id]</td><td>$row[c_name]</td></tr>";
            
    }
 
