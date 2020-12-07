df79ef3ac727fbcacb346179ebe913aa2810b060
zach.gharst <zdgharst@gmail.com>
2020-12-07 16:43:03 -0600

    Modify column formatting for clusters

--- public_html/clusters.php
+++ public_html/clusters.php
@@ -15,18 +15,18 @@ FROM   (SELECT cluster_id,
      FROM   cluster
      GROUP  BY cluster_id) AS first_eq_in_cluster
     JOIN earthquake
-      ON most_recent_earthquake = id";
+      ON most_recent_earthquake = id ORDER BY num_earthquakes DESC";
     $clustersData = mysqli_query($connection, $clustersData);
 
     $title = "Clusters";    
     $content = <<<TABLE
         <table class="right">
             <tr><td colspan="9" class="first">$paging_choices</td></tr>
-            <tr><th class="left">Cluster ID</th><th>Date</th><th>Time</th><th>Magnitude</th><th>Latitude</th><th>Longitude</th><th># of EQ</th></tr>
+            <tr><th class="left">Start Date</th><th>Start Time</th><th>Magnitude</th><th>Latitude</th><th>Longitude</th><th># of EQs</th></tr>
 TABLE;
 
     while($row = mysqli_fetch_assoc($clustersData)) {
-        $content .= "<tr><td><a href=\"earthquakes.php?cluster=$row[cluster_id]\">Cluster $row[cluster_id]</a></td><td>$row[formattedDate]</td><td>$row[formattedTime]</td><td>$row[mag]</td><td>$row[latitude]</td><td>$row[longitude]</td><td>$row[num_earthquakes]</td></tr>";
+        $content .= "<tr><td class=\"left\"><a href=\"earthquakes.php?cluster=$row[cluster_id]\">$row[formattedDate]</a></td><td>$row[formattedTime]</td><td>$row[mag]</td><td>$row[latitude]</td><td>$row[longitude]</td><td>$row[num_earthquakes]</td></tr>";
     }
 
     $content .= <<<TABLE2
