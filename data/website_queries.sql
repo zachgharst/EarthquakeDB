6587997219bbe192ae209faf3e2949a25a6c942d
zach.gharst <zdgharst@gmail.com>
2020-12-09 07:39:42 -0600

    Add the city and insurance queries

--- data/website_queries.sql
+++ data/website_queries.sql
@@ -50,10 +50,10 @@ SELECT DATE_FORMAT(time, '%M %e, %Y') as formattedDate,
   Clusters : clusters.php
  **********************************************/
 
- /* This query is used to return the cluster as well as the oldest earthquake that
-  * belongs to that cluster in order to add some context to the cluster. In the
-  * future, this query could be improved by instead filtering by the oldest
-  * DATETIME stamp rather than the smallest ID. */
+/* This query is used to return the cluster as well as the oldest earthquake that
+ * belongs to that cluster in order to add some context to the cluster. In the
+ * future, this query could be improved by instead filtering by the oldest
+ * DATETIME stamp rather than the smallest ID. */
 SELECT cluster_id,
     Date_format(time, '%M %e, %Y') AS formattedDate,
     Time(time)                     AS formattedTime,
@@ -68,3 +68,46 @@ FROM   (SELECT cluster_id,
      GROUP  BY cluster_id) AS first_eq_in_cluster
     JOIN earthquake
       ON most_recent_earthquake = id ORDER BY num_earthquakes DESC;
+
+/***********************************************
+  Cities : city.php
+ **********************************************/
+
+/* This query is the default view of the page without any variables set. It's used to 
+ * count all cities in the database for pagination purposes. */
+SELECT id, name, latitude, longitude, country, population FROM city ORDER BY population DESC;
+
+/* This is the same query as above, but it considers the pagination (offset & limit). */
+SELECT id, name, latitude, longitude, country, population FROM city ORDER BY population DESC  LIMIT $offset, 200;
+
+/* This query is used when the user searches for a city by city name. */
+SELECT id, name, latitude, longitude, country, population FROM city WHERE name LIKE '%$cityName%' ORDER BY population DESC;
+
+/* This query is used once a city has been selected to display city information; the
+ * only variable needed is the ID. Part 1 of 3 of the Single City Display. */
+SELECT name, latitude, longitude, country, population FROM city WHERE id = $id;
+
+/* Once a city has been found, we gather information about the earthquakes that have
+ * happened within 50 miles. We also state, as a business constraint, one year.
+ * Part 2 of 3 of the Single City Display. */
+SELECT DATE_FORMAT(time, '%M %e, %Y') as time1, TIME(time) as time2, mag, earthquake.latitude, earthquake.longitude, ST_Distance_Sphere(
+                point(earthquake.longitude, earthquake.latitude),
+                point(city.longitude, city.latitude )
+            ) * .000621371192 AS distance  
+            FROM earthquake INNER JOIN city ON city.id = $id
+            HAVING distance < 50
+            ORDER BY distance;
+
+/* The last part of the Single City Display is listing out the insurance policies that are
+ * available in that city. */
+SELECT id, policy_name, companyname FROM policy WHERE city_id = $id;
+
+/***********************************************
+  Insurance Policies : policies.php
+ **********************************************/
+
+/* The insurance policy view is quite self explanatory. We want to know the policy name,
+ * ID, and the city it belongs to (via a join). */
+SELECT policy_name as p_name, policy.id as p_id , city.name as c_name
+    FROM policy inner join city
+    on policy.city_id = city.id;
